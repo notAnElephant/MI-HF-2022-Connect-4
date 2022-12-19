9dba6e910356ec93f75c0c1e6c1f247b10d88d8e
Oliver Raisz <raiszoliver@gmail.com>
2022-12-19 20:25:29 +0100

    minimax implemented with simple heuristics
    able to beat the greedy algorithm

--- python/student_player.py
+++ python/student_player.py
@@ -1,5 +1,6 @@
+import math
+import random
 from typing import List
-
 from board import Board
 
 
@@ -21,10 +22,59 @@ class StudentPlayer:
         """
 
     def step(self, last_player_col: int) -> int:
-        if last_player_col != -1: # if there was a step
+        if last_player_col != -1:  # if the other player made a step, update the board accordingly
             self.__board.step(self._other_player_index, last_player_col)
 
-        col = 0  # TODO your logic here
+        col = self.minimax(self.__board, 2, self.__player_index)[0]
 
         self.__board.step(self.__player_index, col)
         return col
+
+    # implement minimax algorithm, where the current player is the maximizing player
+    # and the desired return value is the column index of the best step
+    def minimax(self, board, remaining_depth, player_index):
+        valid_steps = board.get_valid_steps()
+        col = random.choice(valid_steps)
+        if remaining_depth == 0 or board.game_ended():  # evaluate the current board state
+            return col, self.value_of_board(board, player_index)
+
+        if player_index == self.__player_index:  # our player is the maximizing player
+            highest_score = -math.inf
+            for step in valid_steps:
+                copy_board = board.copy()
+                copy_board.step(player_index, step)
+                possible_score = self.minimax(copy_board, remaining_depth - 1, self._other_player_index)[1]
+                if possible_score > highest_score:
+                    highest_score = possible_score
+                    col = step
+            return col, highest_score
+        else:  # it's the other player's turn, who is the minimizing player
+            lowest_score = math.inf
+            for step in valid_steps:
+                copy_board = board.copy()
+                copy_board.step(player_index, step)
+                possible_score = self.minimax(copy_board, remaining_depth - 1, self.__player_index)[1]
+                if possible_score < lowest_score:
+                    lowest_score = possible_score
+                    col = step
+            return col, lowest_score
+
+    def value_of_board(self, board, player_index):
+        if board.game_ended():
+            if board.get_winner() == 1:
+                return -10000
+            else:
+                return 10000
+        return 0
+        # board_values = board.get_state()
+        # cols = [0, 0, 0, 0, 0, 0, 0]
+        # for i in range(0, 7):
+        #     for j in range(0, 6):
+        #         if board_values[j][i] == 0:
+        #             continue
+        #         if board_values[j][i] == player_index:
+        #             cols[i] += 1
+        #         elif board_values[j][i] != player_index:
+        #             break
+        #
+        # return cols.sum()
